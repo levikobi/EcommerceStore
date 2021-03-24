eae6ef2c7444ddc71218d79645ca1a658bb5bc91
kobi levi <kobilevi503@gmail.com>
2021-03-24 20:44:41 +0200

    Order payment reducer & action

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -5,6 +5,10 @@ import {
     ORDER_DETAILS_REQUEST,
     ORDER_DETAILS_SUCCESS,
     ORDER_DETAILS_FAILURE,
+    ORDER_PAYMENT_REQUEST,
+    ORDER_PAYMENT_SUCCESS,
+    ORDER_PAYMENT_FAILURE,
+    ORDER_PAYMENT_RESET,
 } from "../constants/orderConstants";
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -49,6 +53,29 @@ export const orderDetailsReducer = (
                 loading: false,
                 error: action.payload,
             };
+        default:
+            return state;
+    }
+};
+
+export const orderPayReducer = (state = {}, action) => {
+    switch (action.type) {
+        case ORDER_PAYMENT_REQUEST:
+            return {
+                loading: true,
+            };
+        case ORDER_PAYMENT_SUCCESS:
+            return {
+                loading: false,
+                success: true,
+            };
+        case ORDER_PAYMENT_FAILURE:
+            return {
+                loading: false,
+                error: action.payload,
+            };
+        case ORDER_PAYMENT_RESET:
+            return {};
         default:
             return state;
     }
