8fa21419daac23115696db1d886a850fce11f525
kobi levi <kobilevi503@gmail.com>
2021-03-24 20:22:11 +0200

    Order screen

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -40,7 +40,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
     const order = await Order.findById(req.params.id).populate("user", "name email");
-
+    console.log(order);
     if (order) {
         res.json(order);
     } else {
