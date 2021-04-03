ec349518f7253fe2015c2524d084b1bd50f143ff
kobi levi <kobilevi503@gmail.com>
2021-04-03 16:55:35 +0300

    Admin user delete endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,11 +6,13 @@ import {
     getUserProfile,
     updateUserProfile,
     getUsers,
+    deleteUser,
 } from "../controllers/userController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(protect, admin, getUsers).post(registerUser);
 router.route("/login").post(authUser);
 router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
+router.route("/:id").delete(protect, admin, deleteUser);
 
 export default router;
