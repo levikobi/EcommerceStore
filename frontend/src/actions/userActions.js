dd8e4ec91d5cd662fc37e3de392dd6f9f3c923d1
kobi levi <kobilevi503@gmail.com>
2021-04-14 17:08:25 +0300

    Update user functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -21,6 +21,9 @@ import {
     USER_DELETE_REQUEST,
     USER_DELETE_SUCCESS,
     USER_DELETE_FAILURE,
+    USER_UPDATE_REQUEST,
+    USER_UPDATE_SUCCESS,
+    USER_UPDATE_FAILURE,
 } from "../constants/userConstants";
 import { ORDER_LIST_MY_RESET } from "../constants/orderConstants";
 
@@ -222,6 +225,38 @@ export const deleteUser = (id) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: USER_DELETE_FAILURE,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
+
+export const updateUser = (user) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_UPDATE_REQUEST,
+        });
+
+        const {
+            userLogin: { userInfo },
+        } = getState();
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        };
+
+        const { data } = await axios.put(`/api/users/${user._id}`, user, config);
+
+        dispatch({ type: USER_UPDATE_SUCCESS });
+        dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch({
+            type: USER_UPDATE_FAILURE,
             payload:
                 error.response && error.response.data.message
                     ? error.response.data.message
--- frontend/src/screens/UserEditScreen.jsx
+++ frontend/src/screens/UserEditScreen.jsx
@@ -5,9 +5,10 @@ import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 import FormContainer from "../components/FormContainer";
-import { getUserDetails } from "../actions/userActions";
+import { getUserDetails, updateUser } from "../actions/userActions";
+import { USER_UPDATE_RESET } from "../constants/userConstants";
 
-function UserEditScreen({ match, location }) {
+function UserEditScreen({ match, history }) {
     const userId = match.params.id;
 
     const [name, setName] = useState("");
@@ -19,18 +20,25 @@ function UserEditScreen({ match, location }) {
     const userDetails = useSelector((state) => state.userDetails);
     const { loading, error, user } = userDetails;
 
+    const userUpdate = useSelector((state) => state.userUpdate);
+    const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = userUpdate;
+
     useEffect(() => {
-        if (!user.name || user._id !== userId) {
+        if (successUpdate) {
+            dispatch({ type: USER_UPDATE_RESET });
+            history.push("/admin/userlist");
+        } else if (!user.name || user._id !== userId) {
             dispatch(getUserDetails(userId));
         } else {
             setName(user.name);
             setEmail(user.email);
             setIsAdmin(user.isAdmin);
         }
-    }, [dispatch, userId, user]);
+    }, [dispatch, history, userId, user, successUpdate]);
 
     const submitHandler = (e) => {
         e.preventDefault();
+        dispatch(updateUser({ _id: userId, name, email, isAdmin }));
     };
 
     return (
@@ -40,6 +48,8 @@ function UserEditScreen({ match, location }) {
             </Link>
             <FormContainer>
                 <h1>Edit User</h1>
+                {loadingUpdate && <Loader />}
+                {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
                 {loading ? (
                     <Loader />
                 ) : error ? (
