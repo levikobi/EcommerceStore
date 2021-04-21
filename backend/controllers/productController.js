c57cf88a87068c040a1316e208c213ca9de43fd9
kobi levi <kobilevi503@gmail.com>
2021-04-21 14:48:11 +0300

    create product endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -36,4 +36,24 @@ const deleteProduct = asyncHandler(async (req, res) => {
     }
 });
 
+// @desc    Create single product
+// @route   POST /api/products
+// @access  Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+    const product = new Product({
+        name: "Sample Name",
+        price: 0,
+        user: req.user._id,
+        image: "/images/sample.jpg",
+        brand: "Sample Brand",
+        category: "Sample Category",
+        countInStock: 0,
+        numReviews: 0,
+        description: "Sample Description",
+    });
+
+    const createdProduct = await product.save();
+    res.status(201).json(product);
+});
+
 export { getProducts, getProductById, deleteProduct };
