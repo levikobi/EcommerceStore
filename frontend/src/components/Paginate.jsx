4e768bdf3a2ce783542cbf7bc489acadfcc7b7e6
kobi levi <kobilevi503@gmail.com>
2021-04-24 16:13:15 +0300

    page numbers for product list ui

--- frontend/src/components/Paginate.jsx
+++ frontend/src/components/Paginate.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Pagination } from "react-bootstrap";
-import { Link, LinkContainer } from "react-router-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 function Paginate({ pages, page, isAdmin = false, keyword = "" }) {
     return (
