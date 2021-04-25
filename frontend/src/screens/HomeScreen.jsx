610db2c867be37770fc4d254d0f772acedb5bfc3
kobi levi <kobilevi503@gmail.com>
2021-04-25 08:32:36 +0300

    top products carousel component

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -5,6 +5,7 @@ import Product from "../components/Product";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 import Paginate from "../components/Paginate";
+import ProductCarousel from "../components/ProductCarousel";
 import { listProducts } from "../actions/productActions";
 
 function HomeScreen({ match }) {
@@ -22,6 +23,7 @@ function HomeScreen({ match }) {
 
     return (
         <>
+            {!keyword && <ProductCarousel />}
             <h1>Latests Products</h1>
             {loading ? (
                 <Loader />
